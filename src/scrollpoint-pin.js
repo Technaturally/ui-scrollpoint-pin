c2f9175ab180caedb58191ff7f4ed044419b520f
Pat Long <plong00@gmail.com>
2015-12-31 16:40:15 -0800

    using a controller

--- src/scrollpoint-pin.js
+++ src/scrollpoint-pin.js
@@ -1,107 +1,137 @@
 angular.module('ui.scrollpoint.pin', ['ui.scrollpoint'])
-.directive('uiScrollpointPin', [function () {
+.directive('uiScrollpointPin', [function(){
     return {
         restrict: 'A',
-        require: 'uiScrollpoint',
-        link: function (scope, elm, attrs , uiScrollpoint){
-            var placeholder;
-            var offset = {};
+        require: ['uiScrollpoint', 'uiScrollpointPin'],
+        controller: [function(){
+            var self = this;
+            this.$element;
+            this.$placeholder;
+            this.$uiScrollpoint;
+
+            this.edge;
+            this.offset = {};
+
             var origCss = {};
             var pinToTarget = false;
 
-            function repositionPinned(){
-                if(placeholder){
-                    var element = elm;
-                    var scrollOffset = uiScrollpoint.hasTarget ? 0 : uiScrollpoint.getScrollOffset();
-                    element.css('left', (offset.x)+'px');
-                    element.css('top', (scrollOffset-offset.y)+'px');
+            this.repositionPinned = function(){
+                if(self.$placeholder && self.$uiScrollpoint){
+                    var scrollOffset = self.$uiScrollpoint.hasTarget ? 0 : self.$uiScrollpoint.getScrollOffset();
+                    self.$element.css('left', (self.offset.x)+'px');
+                    self.$element.css('top', (scrollOffset-self.offset.y)+'px');
                 }
-            }
-            function pin(edge, distance){
-                if(!placeholder){
-                    var element = elm;
+            };
+
+            this.setElement = function(element){
+                this.$element = element;
+            };
+            this.setScrollpoint = function(uiScrollpoint){
+                this.$uiScrollpoint = uiScrollpoint;
+            };
+
+            this.pin = function(edge, distance){
+                if(!this.$placeholder && this.$element && this.$uiScrollpoint){
                     // calculate the offset for its absolute positioning
-                    offset.x = element[0].offsetLeft;
-                    offset.y = uiScrollpoint.getScrollOffset() - element[0].offsetTop - distance * ((edge == 'bottom')?-1.0:1.0);
+                    this.offset.x = this.$element[0].offsetLeft;
+                    this.offset.y = this.$uiScrollpoint.getScrollOffset() - this.$element[0].offsetTop - distance * ((edge == 'bottom')?-1.0:1.0);
 
                     // create an invisible placeholder
-                    placeholder = element.clone();
-                    placeholder.addClass('placeholder');
-                    placeholder.css('visibility', 'hidden');
-                    element.after(placeholder);
+                    this.$placeholder = this.$element.clone();
+                    this.$placeholder.addClass('placeholder');
+                    this.$placeholder.css('visibility', 'hidden');
+                    this.$element.after(this.$placeholder);
 
                     // pin to ui-scrollpoint-target if the parent is not the target
-                    pinToTarget = (uiScrollpoint.hasTarget && uiScrollpoint.$target != element.parent());
+                    pinToTarget = (this.$uiScrollpoint.hasTarget && this.$uiScrollpoint.$target != this.$element.parent());
                     if(pinToTarget){
-                        var bounds = element[0].getBoundingClientRect();
-                        var targetBounds = uiScrollpoint.$target[0].getBoundingClientRect();
+                        var bounds = this.$element[0].getBoundingClientRect();
+                        var targetBounds = this.$uiScrollpoint.$target[0].getBoundingClientRect();
 
-                        offset.x = bounds.left;
-                        offset.y = -uiScrollpoint.$target[0].offsetTop - (bounds.top-targetBounds.top+distance);
+                        this.offset.x = bounds.left;
+                        this.offset.y = -(this.$uiScrollpoint.$target[0].offsetTop) - (bounds.top-targetBounds.top+distance);
 
                         if(edge == 'bottom'){
-                            offset.y = -uiScrollpoint.$target[0].offsetTop - uiScrollpoint.$target[0].offsetHeight + element[0].offsetHeight - (bounds.bottom-targetBounds.bottom-distance);
+                            this.offset.y = -(this.$uiScrollpoint.$target[0].offsetTop) - this.$uiScrollpoint.$target[0].offsetHeight + this.$element[0].offsetHeight - (bounds.bottom-targetBounds.bottom-distance);
                         }
 
-                        uiScrollpoint.$target.append(element);
+                        this.$uiScrollpoint.$target.append(this.$element);
                     }
 
                     // save the css properties that get modified by pinning functions
-                    origCss.position = element[0].style.position; //element.css('position');
-                    origCss.top = element[0].style.top; //element.css('top');
-                    origCss.left = element[0].style.left; //element.css('left');
-                    origCss.width = element[0].style.width;
+                    origCss.position = this.$element[0].style.position; //element.css('position');
+                    origCss.top = this.$element[0].style.top; //element.css('top');
+                    origCss.left = this.$element[0].style.left; //element.css('left');
+                    origCss.width = this.$element[0].style.width;
+
 
                     // lock the width at whatever it is before pinning (since absolute positioning could take it out of context)
-                    element.css('width', element[0].offsetWidth+'px');
+                    this.$element.css('width', this.$element[0].offsetWidth+'px');
 
                     // pin the element
-                    element.addClass('pinned');
-                    element.css('position', 'absolute');
+                    this.$element.addClass('pinned');
+                    this.$element.css('position', 'absolute');
+
+                    // keep track of which edge
+                    this.edge = edge;
 
                     // adjust the element's absolute top whenever target scrolls
-                    uiScrollpoint.$target.on('scroll', repositionPinned);
-                    repositionPinned();
+                    this.$uiScrollpoint.$target.on('scroll', self.repositionPinned);
+                    self.repositionPinned();
                 }
-            }
-            function unpin(){
-                if(placeholder){
-                    var element = elm;
+            };
+
+            this.unpin = function(){
+                if(this.$placeholder && this.$element && this.$uiScrollpoint){
+
                     // stop adjusting absolute position when target scrolls
-                    uiScrollpoint.$target.off('scroll', repositionPinned);
+                    this.$uiScrollpoint.$target.off('scroll', self.repositionPinned);
 
                     // reset element to unpinned state
-                    element.removeClass('pinned');
+                    this.$element.removeClass('pinned');
                     for(var prop in origCss){
-                        element.css(prop, origCss[prop]);
+                        this.$element.css(prop, origCss[prop]);
                     }
 
                     if(pinToTarget){
-                        placeholder.after(element);
+                        this.$placeholder.after(this.$element);
                     }
 
-                    // destroy the placeholder
-                    placeholder.remove();
-                    placeholder = undefined;
+                    var edge = this.edge;
+                    this.edge = undefined;
 
-                    uiScrollpoint.cachePosition();
+                    this.offset = {};
+
+                    // destroy the placeholder
+                    this.$placeholder.remove();
+                    this.$placeholder = undefined;
+
+                    this.$uiScrollpoint.cachePosition();
                 }
-            }
+            };
+
+        }],
+        link: function (scope, elm, attrs , Ctrl){
+            var uiScrollpoint = Ctrl[0];
+            var uiScrollpointPin = Ctrl[1];
+
+            uiScrollpointPin.setElement(elm);
+            uiScrollpointPin.setScrollpoint(uiScrollpoint);
 
             attrs.$observe('uiScrollpointEnabled', function(scrollpointEnabled){
                 scrollpointEnabled = scope.$eval(scrollpointEnabled);
                 if(!scrollpointEnabled){
-                    unpin();
+                    uiScrollpointPin.unpin();
                 }
             });
 
             // create a scrollpoint action that pins the element
             uiScrollpoint.addAction(function(distance, element, edge){
-                if(distance >= 0 && !placeholder){
-                    pin(edge, distance);
+                if(distance >= 0 && !uiScrollpointPin.$placeholder){
+                    uiScrollpointPin.pin(edge, distance);
                 }
-                else if(distance < 0 && placeholder){
-                    unpin();
+                else if(distance < 0 && uiScrollpointPin.$placeholder){
+                    uiScrollpointPin.unpin();
                 }
             });
         }
